b4c81c2e30af4f5e66e1a37ff43974200805f8f2
ToonVanstreels <vanstreels.toon@gmail.com>
2024-05-26 16:14:38 +0000

    countdown of each event and formatting

--- clock_V1.py
+++ clock_V1.py
@@ -1,45 +1,46 @@
 import streamlit as st
 from datetime import datetime, timedelta
 import time
+import pytz
 
 # Set the page configuration to use the entire screen width
 st.set_page_config(layout="wide")
 
-# Set the title of the web app
-#st.title('RedAnt Racing Event Clock')
+# Define the desired time zone
+time_zone = pytz.timezone('Europe/Paris')  # Change to your desired time zone
 
 # Function to get current time and date
 def get_current_time():
-    return datetime.now().strftime('%H:%M:%S')
+    return datetime.now(time_zone).strftime('%H:%M:%S')
 
 def get_current_date():
-    return datetime.now().strftime("%A, %B %d, %Y")
+    return datetime.now(time_zone).strftime("%A, %B %d, %Y")
 
 # Function to update the event list
 def update_event_list(events):
-    current_time = datetime.now()
+    current_time = datetime.now(time_zone)
     upcoming_events = [(name, date) for name, date in events if date > current_time]
     return upcoming_events
 
 # Function to get the next event and time remaining
 def get_next_event(events):
-    current_time = datetime.now()
+    current_time = datetime.now(time_zone)
     for event_name, event_date in events:
         if event_date > current_time:
             return event_name, event_date - current_time
     return None, None
 
-# List of events
+# List of events with time zone information
 events = [
-    ("Trackwalk start", datetime(2024, 5, 28, 19, 0, 0)),
-    ("Trackwalk end ", datetime(2024, 5, 28, 20, 0, 0)),
-    ("Pitlane open", datetime(2024, 5, 11, 10, 10, 0)),
-    ("Race 1 start", datetime(2024, 5, 11, 10, 30, 0)),
-    ("Race 1 end ", datetime(2024, 5, 11, 11, 5, 0))
+    ("Trackwalk start", time_zone.localize(datetime(2024, 5, 26, 19, 0, 0))),
+    ("Trackwalk end", time_zone.localize(datetime(2024, 5, 26, 20, 0, 0))),
+    ("Pitlane open", time_zone.localize(datetime(2024, 5, 11, 10, 10, 0))),
+    ("Race 1 start", time_zone.localize(datetime(2024, 5, 11, 10, 30, 0))),
+    ("Race 1 end", time_zone.localize(datetime(2024, 5, 11, 11, 5, 0)))
 ]
 
 # Create columns for time of day and countdown
-col_time, col_countdown = st.columns([3, 4])
+col_time, col_countdown = st.columns([2, 2])
 
 with col_time:
     time_placeholder = st.empty()
@@ -71,23 +72,34 @@ def update():
     event_names = [name for name, date in upcoming_events]
     event_dates = [date.strftime('%A %Y-%m-%d') for name, date in upcoming_events]
     event_times = [date.strftime('%H:%M') for name, date in upcoming_events]
+    event_countdowns = [(date - datetime.now(time_zone)).total_seconds() for name, date in upcoming_events]
     
-    col1, col2, col3 = st.columns([3,3,1])
+    col1, col2, col3, col4 = st.columns([2, 2, 1, 1])
     
     with col1:
         st.markdown("<h2 style='margin-bottom: 0px;'><u>Event Name</u></h2>", unsafe_allow_html=True)
         for event in event_names:
-            st.markdown(f"<p style='font-size:30px; margin-bottom: 0px;'>{event}</p>", unsafe_allow_html=True)
+            st.markdown(f"<p style='font-size:40px; margin-bottom: 0px;'>{event}</p>", unsafe_allow_html=True)
     
     with col2:
         st.markdown("<h2 style='margin-bottom: 0px;'><u>Date</u></h2>", unsafe_allow_html=True)
         for date in event_dates:
-            st.markdown(f"<p style='font-size:30px; margin-bottom: 0px;'>{date}</p>", unsafe_allow_html=True)
+            st.markdown(f"<p style='font-size:40px; margin-bottom: 0px;'>{date}</p>", unsafe_allow_html=True)
     
     with col3:
         st.markdown("<h2 style='margin-bottom: 0px;'><u>Time</u></h2>", unsafe_allow_html=True)
         for time in event_times:
-            st.markdown(f"<p style='font-size:30px; margin-bottom: 0px;'>{time}</p>", unsafe_allow_html=True)
+            st.markdown(f"<p style='font-size:40px; margin-bottom: 0px;'>{time}</p>", unsafe_allow_html=True)
+
+    with col4:
+        st.markdown("<h2 style='margin-bottom: 0px;'><u>Countdown</u></h2>", unsafe_allow_html=True)
+        for time in event_countdowns:
+            if time > 0:
+                hours, remainder = divmod(int(time), 3600)
+                minutes, seconds = divmod(remainder, 60)
+                st.markdown(f"<p style='font-size:40px; margin-bottom: 0px;'>{hours:02d}:{minutes:02d}:{seconds:02d}</p>", unsafe_allow_html=True)
+            else:
+                st.markdown(f"<p style='font-size:40px; margin-bottom: 0px;'>00:00:00</p>", unsafe_allow_html=True)
 
 # Initial call to update
 update()
